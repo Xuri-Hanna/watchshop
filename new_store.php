03a4693363d8e65b11f0abfd5625fd765a74d177
Xuri-Hanna <phamtrunghieu04112003@gmail.com>
2024-08-27 09:49:19 +0700

    Update new_store.php

--- new_store.php
+++ new_store.php
@@ -1,8 +1,5 @@
 <?php
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "watchshop.mysql.database.azure.com", "watchshop", "Hieu0411!", "watchess", 3306, MYSQLI_CLIENT_SSL);
-
+$con = mysqli_connect("watchshop.mysql.database.azure.com","watchshop","Hieu0411!","watchess");
 session_start();
 if(isset($_SESSION['user']))
 {
